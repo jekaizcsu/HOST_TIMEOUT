df9075286070e6502d5ede0e1081521d62717272
evb <jekaizcsu@gmail.com>
2024-09-05 14:12:55 -0400

    fallocate command with sudo prefix

--- file_block.sh
+++ file_block.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 read -p "Enter file name:" file_name
 read -p "Enter size block in KB:" size_block
-fallocate -l "$size_block"KB /home/evb/$file_name
+sudo fallocate -l "$size_block"KB /home/evb/$file_name
