9f755385cdc6859d0bcef58a7a9565d907c100d7
evb <jekaizcsu@gmail.com>
2024-09-06 16:09:26 -0400

    Used /home/evb for created file

--- file_block.sh
+++ file_block.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 read -p "Enter file name:" file_name
 read -p "Enter size block in KB:" size_block
-sudo fallocate -l "$size_block"KB /home/evb/$file_name
+cd $HOME
+fallocate -l "$size_block"KB $file_name
