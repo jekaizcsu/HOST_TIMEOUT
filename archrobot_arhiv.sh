94338bcaf587563d5987ca4aac1e17d8aafca4f6
evb <jekaizcsu@gmail.com>
2024-09-06 15:37:12 -0400

    tar+bzip

--- archrobot_arhiv.sh
+++ archrobot_arhiv.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 DATES=$(date +%Y_%m_%d)
-sudo tar cf /home/archrobot/date_$DATES.tar /home/qbotfid/qbot/*.txt /home/qbotfid/qbot/*.log /home/qbotfid/qbot/*.out
-sudo gzip /home/archrobot/date_$DATES.tar
+tar czf /home/archrobot/date_$DATES.tar.gz /home/qbotfid/qbot/*.txt /home/qbotfid/qbot/*.log /home/qbotfid/qbot/*.out
